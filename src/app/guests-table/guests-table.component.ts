b5ee300f477f5f2b28586ed79c6d9f82e83694fb
Volodymyr Chygura <volodymyr.chygura@gmail.com>
2018-06-27 18:20:08 +0300

    added get methods in guests-table component

--- src/app/guests-table/guests-table.component.ts
+++ src/app/guests-table/guests-table.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Guest } from '../../classes/guest';
+import { GuestService } from '../guest.service';
+
+import { PropertyTemplate } from '../../classes/propertyTemplate';
+import { PropertyTemplateService } from '../property-template.service';
+
 @Component({
   selector: 'app-guests-table',
   templateUrl: './guests-table.component.html',
   styleUrls: ['./guests-table.component.css']
 })
 export class GuestsTableComponent implements OnInit {
+  guests: Guest[];
+  propertyTemplates: PropertyTemplate[];
 
-  constructor() { }
+  constructor(
+    private guestService: GuestService,
+    private propertyTemplateService: PropertyTemplateService
+  ) {
+  }
 
   ngOnInit() {
+    this.getGuests();
+    this.getPropertyTemplates();
+    console.log(this.guests);
+    console.log(this.propertyTemplates);
+  }
+
+  getGuests(): void {
+    this.guestService.getGuests()
+      .subscribe(guests => this.guests = guests);
+  }
+
+  getPropertyTemplates(): void {
+    this.propertyTemplateService.getPropertyTemplates()
+      .subscribe(templates => this.propertyTemplates = templates);
   }
 
 }
